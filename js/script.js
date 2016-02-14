2a293beac5436dc6e5b0333e4aa3333bf96d18b9
Muneer Hudha <muneerulhudha@gmail.com>
2016-02-13 19:42:16 -0600

    Drag and drop API updated

--- js/script.js
+++ js/script.js
@@ -21,3 +21,17 @@ function showPosition(position) {
     x.innerHTML="Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;	
 }
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}

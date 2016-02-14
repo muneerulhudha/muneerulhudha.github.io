1aae720bf35ed1a4bbdf310cb1900042cb8f78b5
Muneer Hudha <muneerulhudha@gmail.com>
2016-02-13 18:47:07 -0600

    Assignment answers

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ $(function(){
     $('.homePage').css({ height: ($(window).innerHeight() - 100) });
   });
 });
+
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML="Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;	
+}
